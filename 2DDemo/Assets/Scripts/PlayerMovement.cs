79341365dee742b5dcff9cf248ae8f24b4bd3e76
JohnnyJohnJohns <johnhealy123@gmail.com>
2021-02-22 11:45:28 +0000

    Created basic player movement and test platforms

--- 2DDemo/Assets/Scripts/PlayerMovement.cs
+++ 2DDemo/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+    private bool canJump;
+
+    private void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        var moveAxis = Vector3.right * moveDir;
+
+        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDir = context.ReadValue<float>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump)
+        {
+            myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
     }
 }
