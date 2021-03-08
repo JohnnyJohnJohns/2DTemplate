956edf6f0d699974aeaea2d4f9c7b7fddb1880f7
JohnnyJohnJohns <johnhealy123@gmail.com>
2021-03-08 12:09:40 +0000

    Added basic enemy AI, UI and shooting.

--- 2DDemo/Assets/Scripts/PlayerMovement.cs
+++ 2DDemo/Assets/Scripts/PlayerMovement.cs
@@ -56,6 +56,11 @@ public class PlayerMovement : MonoBehaviour
         moveDir = context.ReadValue<float>();
     }
 
+    public void Move(float moveAmt)
+    {
+        moveDir = moveAmt;
+    }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (canJump)
